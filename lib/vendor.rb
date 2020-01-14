9021823e05183e41f6cf8362156ac2306324f498
Jordan <jordan.wallace.williams@gmail.com>
2020-01-14 09:24:57 -0700

    Update item and vendor methods, and associated vendor tests

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,5 +1,44 @@
 class Vendor
-  def initialize
-    
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    0 if @inventory[item].nil?
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
   end
 end
+
+
+# The Market will need to keep track of its Vendors and their Items. Each Vendor will be able to report its total inventory, stock items, and return the quantity of items. Any item not in stock should return `0` by default.
+
+# Use TDD to create a `Vendor` class that responds to the following interaction pattern:
+
+
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+
+# pry(main)> vendor.stock(item1, 30)
+
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+
+# pry(main)> vendor.stock(item1, 25)
+
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+
+# pry(main)> vendor.stock(item2, 12)
+
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
+# ```
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,11 +1,37 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require './lib/item'
 
 class VendorTest < Minitest::Test
   def test_vendor_exists
-    vendor = Vendor.new
+    vendor = Vendor.new('Rocky Mountain Fresh')
 
     assert_instance_of Vendor, vendor
+  end
+
+  def test_vendor_can_have_attributes
+    vendor = Vendor.new('Rocky Mountain Fresh')
+
+    assert_equal 'Rocky Mountain Fresh', vendor.name
+    assert_equal ({}), vendor.inventory
+  end
+
+  def test_vendor_can_check_stock
+    vendor = Vendor.new('Rocky Mountain Fresh')
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    assert_equal 0, vendor.check_stock(item1)
+  end
+
+  def test_vendor_can_stock_item
+    vendor = Vendor.new('Rocky Mountain Fresh')
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    vendor.stock(item1, 30)
+
+    assert_equal 30, vendor.check_stock(item1)
   end
 end
