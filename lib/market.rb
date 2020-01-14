2aa5d96b1a143ef06b5bb7d541c347e3d23a16d2
Jordan <jordan.wallace.williams@gmail.com>
2020-01-14 10:42:27 -0700

    Add availability_to_sell helper method and framework for sell method, as well as associated tests

--- lib/market.rb
+++ lib/market.rb
@@ -38,4 +38,57 @@ class Market
       item_hash
     end
   end
+
+  def sell(item, amount)
+    availability_to_sell(item, amount)
+  end
+
+  def availability_to_sell(item, amount)
+    available = total_inventory
+    available[item] >= amount
+  end
 end
+
+
+# Add a method to your Market class called `sell` that takes an item and a quantity as arguments.
+# There are two possible outcomes of the `sell` method:
+
+# 1. If the Market does not have enough of the item in stock to satisfy the given quantity, this 
+# method should return `false`.
+
+# 2. If the Market's has enough of the item in stock to satisfy the given quantity,
+# this method should return `true`. Additionally, this method should reduce the stock of the Vendors.
+# It should look through the Vendors in the order they were added and sell the item from the first Vendor 
+# with that item in stock. If that Vendor does not have enough stock to satisfy the given quantity,
+# the Vendor's entire stock of that item will be depleted, and the remaining quantity will be sold 
+# from the next vendor with that item in stock. It will follow this pattern until the entire quantity
+# requested has been sold.
+
+# For example, suppose vendor1 has 35 `peaches` and vendor3 has 65 `peaches`, and vendor1 was
+# added to the market first. If the method `sell(<ItemXXX, @name = 'Peach'...>, 40)` is called,
+# the method should return `true`, vendor1's new stock of `peaches` should be 0, and vendor3's new stock
+# of `peaches` should be 60.
+
+# Use TDD to update the `Market` class so that it responds to the following interaction pattern:
+
+# pry(main)> market.sell(item1, 200)
+# #=> false
+
+# pry(main)> market.sell(item5, 1)
+# #=> false
+
+# pry(main)> market.sell(item4, 5)
+# #=> true
+
+# pry(main)> vendor2.check_stock(item4)
+# #=> 45
+
+# pry(main)> market.sell(item1, 40)
+# #=> true
+
+# pry(main)> vendor1.check_stock(item1)
+# #=> 0
+
+# pry(main)> vendor3.check_stock(item1)
+# #=> 60
+# ```
