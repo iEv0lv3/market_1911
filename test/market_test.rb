434e088890060f1e4e72a30e259c16a253420f77
Jordan <jordan.wallace.williams@gmail.com>
2020-01-14 10:19:19 -0700

    Add tests for sorted_item_list and total_inventory methods

--- test/market_test.rb
+++ test/market_test.rb
@@ -134,4 +134,39 @@ class MarketTest < Minitest::Test
     assert_includes(market.sorted_item_list, 'Peach-Raspberry Nice Cream')
     assert_includes(market.sorted_item_list, 'Tomato')
   end
+
+  def test_market_can_list_total_inventory
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack") 
+
+    vendor3.stock(item1, 65)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expected = market.total_inventory.values_at(item1)[0]
+    expected2 = market.total_inventory.values_at(item2)[0]
+    expected3 = market.total_inventory.values_at(item4)[0]
+    expected4 = market.total_inventory.values_at(item3)[0]
+
+    assert_equal 100, expected
+    assert_equal 7, expected2
+    assert_equal 50, expected3
+    assert_equal 25, expected4
+  end
 end
