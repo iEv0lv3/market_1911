dc67f0ae7baf1638f49066cfa3bfb6ee36045e1b
Jordan <jordan.wallace.williams@gmail.com>
2020-01-14 10:18:46 -0700

    Add sorted_item_list and total_inventory methods

--- lib/market.rb
+++ lib/market.rb
@@ -21,19 +21,21 @@ class Market
       vendor if vendor.inventory.include?(item)
     end
   end
+
+  def sorted_item_list
+    @vendors.map do |vendor|
+      vendor.inventory.map do |item|
+        item[0].name
+      end
+    end.flatten.uniq
+  end
+
+  def total_inventory
+    @vendors.reduce(Hash.new(0)) do |item_hash, vendor|
+      vendor.inventory.map do |item|
+        item_hash[item[0]] += item[1]
+      end
+      item_hash
+    end
+  end
 end
-
-
-# Add a method to your `Market` class called `sorted_item_list` that returns a list of names of all items the Vendors have in stock, sorted alphabetically. This list should not include any duplicate items.
-
-# Additionally, your `Market` class should have a method called `total_inventory` that reports the quantities of all items sold at the market. Specifically, it should return a hash with items as keys and quantities as values. If multiple Vendors sell the same item, the quantity listed should be the sum of all the quantities.
-
-# Use TDD to update your `Market` class so that it responds to the following interaction pattern:
-
-
-# pry(main)> market.sorted_item_list
-# #=> ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
-
-# pry(main)> market.total_inventory
-# #=> {#<Item:0x007f9c56740d48...> => 100, #<Item:0x007f9c565c0ce8...> => 7, #<Item:0x007f9c56343038...> => 50, #=> #<Item:0x007f9c562a5f18...> => 25}
-# ```
